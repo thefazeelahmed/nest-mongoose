a04060eb4a286fb29d48af4858a6e9cf4dfde3ed
Fazeel Ahmed <thefazeelahmed@gmail.com>
2023-02-17 12:58:15 +0500

    envs setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from 'common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
